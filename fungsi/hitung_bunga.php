d4c5e33f43e67365979bfe6d5891403a14f694ba
Daimus <daimus.suudi@gmail.com>
2018-03-29 23:59:28 +0700

    hitung bunga simpan & pinjam

--- fungsi/hitung_bunga.php
+++ fungsi/hitung_bunga.php
@@ -11,7 +11,7 @@ $sel_tgl_hitung = $rt['tgl_hitung'];
 
 $listAnggota = mysqli_query ($koneksi, "SELECT kode_anggota FROM t_simpan GROUP BY kode_anggota");
 while ($row = $listAnggota->fetch_assoc()) {
-	// Hitung Bunga Pinjam
+	// Hitung Bunga Simpan
 	$qselsih_simpan = mysqli_query ($koneksi, "SELECT DATEDIFF('$tgl_sekarang', tgl_masuk) AS selisih FROM t_anggota WHERE kode_anggota='$row[kode_anggota]'");
 	$rselisih_simpan = mysqli_fetch_array($qselsih_simpan, MYSQLI_ASSOC);
 
@@ -21,7 +21,7 @@ while ($row = $listAnggota->fetch_assoc()) {
 	if ($rselisih_simpan['selisih'] < 30){
 		$bunga_simpan = ceil(0.0005 * $rselisih_simpan['selisih'] * $rsaldo_simpan['saldo']);
 		if ($bunga_simpan > 0){
-			$qSimpan = mysqli_query ($koneksi, "INSERT INTO t_simpan (kode_simpan, kode_jenis_simpan, kode_anggota, tgl_simpan, besar_simpanan, u_entry, tgl_entri) VALUES('','1051','$row[kode_anggota]', CURDATE() ,'$bunga_simpan','$u_entry', CURDATE())");	
+			$qSimpan = mysqli_query ($koneksi, "INSERT INTO t_simpan (kode_simpan, kode_jenis_simpan, kode_anggota, tgl_simpan, besar_simpanan, u_entry, tgl_entri) VALUES('','1051','$row[kode_anggota]', CURDATE() ,'$bunga_simpan','$u_entry', CURDATE())");
 		}
 	} else {
 		$bunga_simpan = ceil(0.0005 * $sel_tgl_hitung * $rsaldo_simpan['saldo']);
@@ -29,7 +29,10 @@ while ($row = $listAnggota->fetch_assoc()) {
 			$qtambah_bunga_simpan = mysqli_query ($koneksi, "INSERT INTO t_simpan (kode_simpan, kode_jenis_simpan, kode_anggota, tgl_simpan, besar_simpanan, u_entry, tgl_entri) VALUES('','1051','$row[kode_anggota]', CURDATE() ,'$bunga_simpan','$u_entry', CURDATE())");
 		}
 	}
+}
 
+$listAnggota = mysqli_query ($koneksi, "SELECT kode_anggota FROM t_pinjam GROUP BY kode_anggota");
+while ($row = $listAnggota->fetch_assoc()) {
 	// Hitung Bunga Pinjam
 	$qselisih_pinjam = mysqli_query ($koneksi, "SELECT DATEDIFF('$tgl_sekarang', tgl_masuk) AS selisih FROM t_anggota WHERE kode_anggota='$row[kode_anggota]'");
 	$rselisih_pinjam = mysqli_fetch_array($qselisih_pinjam, MYSQLI_ASSOC);
@@ -37,13 +40,17 @@ while ($row = $listAnggota->fetch_assoc()) {
 	$rsaldo_pinjam = mysqli_fetch_array($qsaldo_pinjam, MYSQLI_ASSOC);
 
 	if ($rselisih_pinjam['selisih'] < 30){
-		$bp = ceil(0.005 * $rsp['selisih'] * $rsalp['saldo']);
-		$bunga_pinjam = 0 - $bp;
-		if ($bunga_pinjam < 0){
-			//$qBunga_pinjam = mysqli_query ($koneksi, "INSERT INTO t_pinjam (kode_pinjam, kode_anggota, kode_jenis_pinjam, tgl_pinjam, )")
-		}
+		$bp = ceil(0.005 * $rselisih_pinjam['selisih'] * $rsaldo_pinjam['saldo']);
+		echo $bp;
+		echo "<br>";
+		echo $rselisih_pinjam['selisih'];
+		echo "<br>";
+		echo $rsaldo_pinjam['saldo'];
+		$qBunga_pinjam = mysqli_query ($koneksi, "INSERT INTO t_pinjam VALUES('', '$row[kode_anggota]', '1234', CURDATE(), '$bp', '0', '0', '0', '0', 'admin', CURDATE(), '0')");
 	} else {
 		// Insert Bunga Pinjam
+		$bp = ceil(0.005 * $sel_tgl_hitung * $rsaldo_pinjam['saldo']);
+		$qBunga_pinjam = mysqli_query ($koneksi, "INSERT INTO t_pinjam VALUES('', '$row[kode_anggota]', '1234', CURDATE(), '$bp', '0', '0', '0', '0', 'admin', CURDATE(), '0')");
 	}
 }
 
